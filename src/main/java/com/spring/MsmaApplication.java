bba3ffb195104dee02b3a2629922bea15e2f88b3
Phạm Hoàng Anh <155462810+Kinhart822@users.noreply.github.com>
2025-06-02 13:00:21 +0700

    Update Code to Deploy

--- src/main/java/com/spring/MsmaApplication.java
+++ src/main/java/com/spring/MsmaApplication.java
@@ -1,10 +1,7 @@
 package com.spring;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.JdbcConnectionDetails;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -15,17 +12,5 @@ public class MsmaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MsmaApplication.class, args);
-    }
-
-    @Bean
-    CommandLineRunner commandLineRunner(JdbcConnectionDetails jdbc) {
-        return args -> {
-            System.out.println(
-                    "class: " + jdbc.getClass().getName() + "\n" +
-                    "JDBC URL: " + jdbc.getJdbcUrl() + "\n" +
-                    "Username: " + jdbc.getUsername() + "\n" +
-                    "Password: " + jdbc.getPassword()
-            );
-        };
     }
 }
