ec0b691b027e6c96ae8d0dc8a5c05b574bfb2f6c
Phạm Hoàng Anh <155462810+Kinhart822@users.noreply.github.com>
2025-06-02 14:48:07 +0700

    Update Code to Deploy

--- src/main/java/com/spring/config/FirebaseConfig.java
+++ src/main/java/com/spring/config/FirebaseConfig.java
@@ -12,26 +12,40 @@ import org.springframework.context.annotation.Configuration;
 import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
+
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @Configuration
 public class FirebaseConfig {
     private static final Logger log = LoggerFactory.getLogger(SongServiceImpl.class);
 
-    @Value("${app.firebase-config}")
-    private String firebaseConfig;
+//    @Value("${app.firebase-config}")
+//    private String firebaseConfig;
 
     @PostConstruct
     private void initialize() {
         try {
-            FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredentials(GoogleCredentials.fromStream(
-                            new ClassPathResource(firebaseConfig).getInputStream()))
-                    .build();
-
-            if (FirebaseApp.getApps().isEmpty()) {
-                FirebaseApp.initializeApp(options);
+            String firebaseCredentials = System.getenv("FIREBASE_CONFIG");
+//            FirebaseOptions options = new FirebaseOptions.Builder()
+//                    .setCredentials(GoogleCredentials.fromStream(
+//                            new ClassPathResource(firebaseConfig).getInputStream()))
+//                    .build();
+//
+//            if (FirebaseApp.getApps().isEmpty()) {
+//                FirebaseApp.initializeApp(options);
+//            }
+            if (firebaseCredentials == null || firebaseCredentials.isEmpty()) {
+                throw new IllegalArgumentException("FIREBASE_CREDENTIALS environment variable is not set");
             }
+            GoogleCredentials credentials = GoogleCredentials.fromStream(
+                    new ByteArrayInputStream(firebaseCredentials.getBytes(StandardCharsets.UTF_8)));
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(credentials)
+                    .build();
+            FirebaseApp.initializeApp(options);
+            System.out.println("FirebaseApp initialized successfully");
         } catch (IOException e) {
             log.error("Create FirebaseApp Error", e);
             throw new RuntimeException("Failed to initialize FirebaseApp", e);
